06d1f46c6480264e665ad23a7333e55324418480
Andrija <aglavas11@gmail.com>
2019-07-20 19:31:07 +0200

    Docker changes

--- src/setup.sh
+++ src/setup.sh
@@ -15,4 +15,4 @@ chgrp -R www-data storage bootstrap/cache && \
 touch storage/logs/laravel.log && chmod 775 storage/logs/laravel.log && chown www-data storage/logs/laravel.log
 
 composer install && php artisan key:generate && npm install
-php artisan migrate:fresh --seed && echo "Done..."
+php artisan migrate:fresh --seed && php artisan passport:install && echo "Done..."
